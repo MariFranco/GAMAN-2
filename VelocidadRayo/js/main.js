880bffa8365b90a951128eea06922f290df15f52
MariFranco <mariana.francoa@upb.edu.co>
2018-11-08 21:25:51 -0500

    Creación arreglo de imagenes

--- VelocidadRayo/js/main.js
+++ VelocidadRayo/js/main.js
@@ -8,6 +8,61 @@ function genera_tabla() {
 
   //Crear Iniciales
   var imgs = [];
+  var imgbochas = new Image();
+  imgbochas.src = "./img/iconobochas.png";
+  imgs.push(imgbochas);
+
+  var imgcaballo = new Image();
+  imgcaballo.src = "./img/iconocaballo.png";
+  imgs.push(imgcaballo);
+
+  var imgconsola = new Image();
+  imgconsola.src = "./img/iconoconsola.png";
+  imgs.push(imgconsola);
+
+  var imgdedal = new Image();
+  imgdedal.src = "./img/iconodedal.png";
+  imgs.push(imgdedal);
+
+  var imgflores = new Image();
+  imgflores.src = "./img/iconoflores.png";
+  imgs.push(imgflores);
+
+  var imggafas3d = new Image();
+  imggafas3d.src = "./img/iconogafas3d.png";
+  imgs.push(imggafas3d);
+
+  var imghilo = new Image();
+  imghilo.src = "./img/iconohilo.png";
+  imgs.push(imghilo);
+
+  var imgmaquinadecoser = new Image();
+  imgmaquinadecoser.src = "./img/iconomaquinadecoser.png";
+  imgs.push(imgmaquinadecoser);
+
+  var imgmedalla = new Image();
+  imgmedalla.src = "./img/iconomedalla.png";
+  imgs.push(imgmedalla);
+
+  var imgrey = new Image();
+  imgrey.src = "./img/iconorey.png";
+  imgs.push(imgrey);
+
+  var imgtejer = new Image();
+  imgtejer.src = "./img/iconotejer.png";
+  imgs.push(imgtejer);
+
+  var imgtorre = new Image();
+  imgtorre.src = "./img/iconotorre.png";
+  imgs.push(imgtorre);
+
+  var imgvideojuegos = new Image();
+  imgvideojuegos.src = "./img/iconovideojuegos.png";
+  imgs.push(imgvideojuegos);
+
+  var imgviolin = new Image();
+  imgviolin.src = "./img/iconoviolin.png";
+  imgs.push(imgviolin);
 
   // Crea las hileras de tabla
   for (var i = 0; i < 4; i++) {
