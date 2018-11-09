07e5824dd3f5dfa1d50e520dee9e3a804695058f
MariFranco <mariana.francoa@upb.edu.co>
2018-11-08 21:10:05 -0500

    Creacion de tabla de imagenes

---
+++ VelocidadRayo/js/main.js
@@ -0,0 +1,39 @@
+function genera_tabla() {
+  // Obtener la referencia del elemento body
+  var body = document.getElementsByTagName("body")[0];
+ 
+  // Crea un elemento <table> y un elemento <tbody>
+  var tabla   = document.createElement("table");
+  var tblBody = document.createElement("tbody");
+
+  //Crear Iniciales
+  var imgs = [];
+
+  // Crea las hileras de tabla
+  for (var i = 0; i < 4; i++) {
+    // Crea las celdas
+    var hilera = document.createElement("tr");
+ 
+    for (var j = 0; j < 3; j++) {
+      // Crea un elemento <td> y un nodo de texto, haz que el nodo de
+      // texto sea el contenido de <td>, ubica el elemento <td> al final
+      // de la hilera de la tabla
+      //Crea imagen
+      var imgCelda = new Image();
+      var celda = document.createElement("td");
+      imgCelda.src = "./img/iconobochas.png";
+      celda.appendChild(imgCelda);
+      hilera.appendChild(celda);
+    }
+ 
+    // agrega la hilera al final de la tabla (al final del elemento tblbody)
+    tblBody.appendChild(hilera);
+  }
+ 
+  // posiciona el <tbody> debajo del elemento <table>
+  tabla.appendChild(tblBody);
+  // appends <table> into <body>
+  body.appendChild(tabla);
+  // modifica el atributo "border" de la tabla y lo fija a "2";
+  tabla.setAttribute("border", "2");
+}
